b6359142aa47d0245649390177e67f60f6c2cc59
Gustavo Pinto <gustavo.pinto@zup.com.br>
2022-07-13 17:27:07 -0300

    imprimindo todos os ICPs no prettyprint

--- src/main/java/br/com/stackedu/cdd/Metricas.java
+++ src/main/java/br/com/stackedu/cdd/Metricas.java
@@ -1,41 +1,115 @@
 package br.com.stackedu.cdd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+//TODO: Criar testes para essa classe
 public class Metricas {
 
-    private static Map<String, Integer> dataset = new HashMap<>();
+    private static Map<String, List<ValorICP>> dataset = new HashMap<>();
 
     public static void salvar(String classQualifiedName, String ICP) {
-        String chave = classQualifiedName + "." + ICP;
-        if (dataset.containsKey(chave)) {
-            int contadorAtual = dataset.get(chave);
-            dataset.replace(chave, contadorAtual, contadorAtual + 1);
+        if (dataset.containsKey(classQualifiedName)) {
+            List<ValorICP> valoresAtuais = dataset.get(classQualifiedName);
+
+            List<ValorICP> novosValores = null;
+            if (verificaSeICPJaExiste(ICP, valoresAtuais)) {
+                novosValores = atualizaValoresAtuais(ICP, valoresAtuais);
+            } else {
+                novosValores = adicionaNovoValor(ICP, valoresAtuais);
+            }
+
+            dataset.replace(classQualifiedName, novosValores);
         } else {
-            dataset.put(chave, 1);
+            dataset.put(classQualifiedName, Arrays.asList(new ValorICP(ICP, 1)));
         }
+
+        System.out.println(dataset);
+    }
+
+    private static List<ValorICP> adicionaNovoValor(String ICP, List<ValorICP> valoresAtuais) {
+        List<ValorICP> v = new ArrayList<>(valoresAtuais);
+        v.add(new ValorICP(ICP, 1));
+        return v;
+    }
+
+    private static List<ValorICP> atualizaValoresAtuais(String ICP, List<ValorICP> valoresAtuais) {
+        List<ValorICP> v = new ArrayList<>(valoresAtuais);
+
+        for (int i = 0; i < valoresAtuais.size(); i++) {
+            ValorICP atual = valoresAtuais.get(i);
+
+            if (ICP.equals(atual.getICP())) {
+                ValorICP novoValor = new ValorICP(ICP, atual.getValor() + 1);
+                v.set(i, novoValor);
+            }
+        }
+
+        return v;
+    }
+
+    private static boolean verificaSeICPJaExiste(String ICP, List<ValorICP> valoresAtuais) {
+        boolean verificaSeICPJaExiste = false;
+        for (ValorICP valorICP : valoresAtuais) {
+            if (valorICP.getICP().equals(ICP)) {
+                verificaSeICPJaExiste = true;
+            }
+        }
+        return verificaSeICPJaExiste;
     }
 
     public static String prettyprint() {
         StringBuilder sb = new StringBuilder();
-        Iterator<Entry<String, Integer>> iter = dataset.entrySet().iterator();
+        Iterator<Entry<String, List<ValorICP>>> iter = dataset.entrySet().iterator();
         while (iter.hasNext()) {
-            Entry<String, Integer> entry = iter.next();
+            Entry<String, List<ValorICP>> entry = iter.next();
+            sb.append(entry.getKey());
+            sb.append('[');
 
-            int valorDaMetrica = entry.getValue();
-            if (valorDaMetrica >= Configuracoes.limite()) {
-                sb.append(entry.getKey());
-                sb.append('=');
-                sb.append(valorDaMetrica);
-                if (iter.hasNext()) {
-                    sb.append('\n');
+            List<ValorICP> ICPs = entry.getValue();
+            for (ValorICP ICP : ICPs) {
+                if (ICP.getValor() >= Configuracoes.limite()) {
+                    sb.append(ICP.getICP());
+                    sb.append('=');
+                    sb.append(ICP.getValor());
+                    sb.append(',');
+
+                    if (iter.hasNext()) {
+                        sb.append("\n");
+                    }
                 }
             }
+            sb.append(']');
         }
         return sb.toString();
     }
 
+}
+
+class ValorICP {
+    private final String ICP;
+    private final Integer valor;
+
+    protected ValorICP(String ICP, Integer valor) {
+        this.ICP = ICP;
+        this.valor = valor;
+    }
+
+    protected String getICP() {
+        return ICP;
+    }
+
+    protected Integer getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + ICP + ", " + valor + "]";
+    }
 }
--- src/test/java/br/com/stackedu/cdd/MetricasCDDTest.java
+++ src/test/java/br/com/stackedu/cdd/MetricasCDDTest.java
@@ -1,26 +1,84 @@
 package br.com.stackedu.cdd;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.stackedu.cdd.icp.AnotacaoProcessor;
+import br.com.stackedu.cdd.icp.IfProcessor;
+import br.com.stackedu.cdd.icp.MetodoProcessor;
 import spoon.Launcher;
 
 public class MetricasCDDTest {
 
+    private Launcher spoon;
+
+    @After
+    public void cleanup() {
+        this.spoon = null;
+    }
+
+    @Before
+    public void setup() {
+        this.spoon = new Launcher();
+    }
+
     @Test
-    public void testName() throws Exception {
-        Launcher l = new Launcher();
-        l.getEnvironment().setNoClasspath(true);
-        l.addInputResource(
-                "/home/gustavopinto/workspace/plataforma-treino-lms/src/main/java/br/com/zup/lms/alunos");
+    public void testClasseComUmICPAcimaDoLimite() throws Exception {
+        this.spoon.getEnvironment().setNoClasspath(true);
+        this.spoon.addInputResource(new Resources().buscaArquivo("Aluno.java"));
 
-        AnotacaoProcessor processor = new AnotacaoProcessor();
+        this.spoon.addProcessor(new AnotacaoProcessor());
+        this.spoon.run();
 
-        l.addProcessor(processor);
-        l.run();
         assertNotNull(Metricas.prettyprint());
+
+        assertEquals("br.com.zup.lms.alunos.Aluno[anotação=28]", Metricas.prettyprint());
+    }
+
+    @Test
+    public void testClasseComDoisICPsAcimaDoLimite() throws Exception {
+        this.spoon.getEnvironment().setNoClasspath(true);
+        this.spoon.addInputResource(new Resources().buscaArquivo("Aluno.java"));
+
+        this.spoon.addProcessor(new AnotacaoProcessor());
+        this.spoon.addProcessor(new MetodoProcessor());
+        this.spoon.run();
+
+        assertNotNull(Metricas.prettyprint());
+
+        assertEquals("br.com.zup.lms.alunos.Aluno[anotação=28,metodo=11,]", Metricas.prettyprint());
+    }
+
+    @Test
+    public void testClasseComDoisICPsAcimaEUmICPAbaixoDoLimite() throws Exception {
+        this.spoon.getEnvironment().setNoClasspath(true);
+        this.spoon.addInputResource(new Resources().buscaArquivo("Aluno.java"));
+
+        this.spoon.addProcessor(new AnotacaoProcessor());
+        this.spoon.addProcessor(new MetodoProcessor());
+        this.spoon.addProcessor(new IfProcessor());
+        this.spoon.run();
+
+        assertNotNull(Metricas.prettyprint());
+
+        assertEquals("br.com.zup.lms.alunos.Aluno[anotação=28,metodo=11,]", Metricas.prettyprint());
+    }
+
+    @Test
+    public void testClasseSemICPsAcimaDoLimete() throws Exception {
+        this.spoon.getEnvironment().setNoClasspath(true);
+        this.spoon.addInputResource(new Resources().buscaArquivo("Aluno.java"));
+
+        this.spoon.addProcessor(new IfProcessor());
+        this.spoon.run();
+
+        assertNotNull(Metricas.prettyprint());
+
+        assertEquals("br.com.zup.lms.alunos.Aluno[]", Metricas.prettyprint());
     }
 
 }
